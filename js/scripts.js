4833475f8729e2b171dd1fb97dd6c0ca619bda7f
Catherine Karra and kate hall <kate@>
2016-03-24 14:30:06 -0700

    show survey questions one by one and attach each survey question to its own form

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
+$(document).ready(function() {
+  $("form#surveyQ1").submit(function(event) {
+  var communityInterest= $("select#commind").val();
 
+  if (communityInterest === 'individual') {
+    $("form#surveyQ2").show();
+  } else {
+    $("form#surveyQ3").show();
+  }
+    event.preventDefault();
+  });
+    $("form#surveyQ2").submit(function(event) {
+    var controlInterest= $("select#control").val();
+
+  if (controlInterest === 'some-control') {
+    $(".result-moderate").show();
+  } else {
+    $(".result-conservative").show();
+  }
+    event.preventDefault();
+  });
+
+  $("form#surveyQ3").submit(function(event) {
+  var revolutionInterest= $("select#radical").val();
+
+  if (revolutionInterest === 'revolution') {
+    $(".result-radical").show();
+  } else {
+    $(".result-liberal").show();
+  }
+    event.preventDefault();
+  });
+});
